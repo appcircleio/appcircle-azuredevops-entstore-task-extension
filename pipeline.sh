faf705f4ef2c283989b859383baee85a9b8505f6
Onur Yılmaz <csonuryilmaz@gmail.com>
2025-01-24 18:01:57 +0300

    Revert test codes

--- pipeline.sh
+++ pipeline.sh
@@ -32,8 +32,6 @@ echo "=== Build Package ==="
 echo
 yarn package
 
-BRANCH_NAME="main"
-
 echo "=== Create Extension ==="
 echo
 if [ "$BRANCH_NAME" == "main" ]; then
@@ -50,7 +48,7 @@ echo
 
 echo "=== Publish Extension ==="
 echo
-echo "tfx extension publish --manifest-globs vss-extension.json --overrides-file $configuration --token $PUBLISH_TOKEN"
+tfx extension publish --manifest-globs vss-extension.json --overrides-file $configuration --token "$PUBLISH_TOKEN"
 echo
 
 exit 0
